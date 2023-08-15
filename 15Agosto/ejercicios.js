81314c51d19e146694fe88e1208f8533c1845d8f
Ansear <sebastianarciniegas2003@gmail.com>
2023-08-15 13:04:56 -0500

    Trabajo Clase

--- 15Agosto/ejercicios.js
+++ 15Agosto/ejercicios.js
@@ -1,2 +1,13 @@
 //EJERCICIO 4
 //Funcion de suma de los numero pares y los impares (por aparte), de numeros desde 1 a 578934
+let acuPar = 0
+let acuIpar = 0
+for(i=1;i<=578934;i++){
+    if(i%2==0){
+        acuPar +=i
+    }else{
+        acuIpar+=i
+    }
+}
+console.log("La suma Par: "+acuPar)
+console.log("La suma Impar: "+acuIpar)
