934015d086f6ff4a7599c583410bf1936641959e
sebastianarciniegas2003 <sebastianarciniegas2003@gmail.com>
2023-08-16 23:55:13 -0500

    avances casi finales

--- retos/hackerRanck.js
+++ retos/hackerRanck.js
@@ -1,24 +1,21 @@
 //correr npm install En terminal, para instalar la dependencia de prompt-sync
-
 const prompt = require("prompt-sync")({ sigint: true });
 let word1 = prompt("Ingresa la primer palabra: ")
 let word2 = prompt("Ingresa la segunda palabra: ")
-let pal1 = "",pal2 = ""
-if (word1.length > word2.length){
-    pal1 = word1
-    pal2 = word2
-}else{
-    pal1 = word2
-    pal2 = word1
-}
-let result = ""
-// console.log(pal1.length + "pal2 " + pal2.length)
-for(let i = 0; i <pal1.length; i++){
-    for(let j = 0; j < pal2.length; j++){
-        result = result.concat('',word1[i])
-        break
+function intercalar_cadenas(pa1,pa2) {
+    let i = 0,j = 0
+    let result = []
+    while (i < pa1.length && j < pa2.length){
+        result.push(pa1[i])
+        result.push(pa2[j])
+        i += 1
+        j += 1
     }
-    result = result.concat('',word2[i])
+    console.log(i)
+    console.log(j)
+    result = result.concat(pa1.slice(i))
+    result = result.concat(pa2.slice(j))
+    return result.join('')
     
 }
-console.log(result)
+console.log(intercalar_cadenas(word1,word2))
